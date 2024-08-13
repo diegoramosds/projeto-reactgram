9f5e185c75365c193a1c33c9e28b2f9143da1c4f
Diego <diegoramos3709@gmail.com>
2024-08-13 17:04:09 -0300

    criando middlewares

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router()
+
+// controller
+const { register } = require("../controllers/UserController");
+
+// middlewares
+const validate  = require("../middlewares/handleValidation")
+
+//routes
+
+router.post("/register", validate, register)
+
+module.exports  = router
