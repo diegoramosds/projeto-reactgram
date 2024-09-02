7f9066d79cc7c085db45312a1e85f7d9de462804
Diego <diegoramos3709@gmail.com>
2024-09-02 17:49:28 -0300

    Formulário do register

--- frontend/reactGram/src/components/NavBar.tsx
+++ frontend/reactGram/src/components/NavBar.tsx
@@ -9,15 +9,17 @@ import { Link, NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between p-5 border-b border-zinc-800 bg-zinc-950">
-      <Link to="/">ReactGram</Link>
+    <nav className="flex justify-between items-center p-3 border-b border-zinc-800 bg-zinc-950 md:">
+      <Link to="/" className="text-zinc-100">ReactGram</Link>
       
-      <form className="flex items-center gap-1">
-        <BsSearch />
-        <input type="text" placeholder="Pesquisar"/>
+      <form className="relative flex items-center">
+        <BsSearch className="absolute left-3 top-3 text-zinc-400"/>
+        <input type="text" 
+        placeholder="Pesquisar" 
+        className="pl-10 pr-4 py-2 rounded-md bg-zinc-800 text-zinc-100 border focus:outline-none focus:ring-2 focus:ring-zinc-700 focus:border-transparent md:pl-10 md:pr-4 md:py-2"/>
         </form>
 
-        <ul className="flex gap-6 items-center">
+        <ul className="flex gap-2 items-center text-zinc-100 pr-3 md:gap-6 md:text-lg">
           <li>
             <NavLink to="/"> <BsHouseDoorFill /> </NavLink>
           </li>
--- frontend/reactGram/src/pages/Auth/Register.tsx
+++ frontend/reactGram/src/pages/Auth/Register.tsx
@@ -7,17 +7,19 @@ const Register = () => {
     console.log('oi')
   }
   return (
-    <div>
-      <h1 className="">ReactGram</h1>
-      <p className="">Crie sua conta e comece a capturar e compartilhar momentos inesquecíveis.</p>
-      <form onSubmit={handleSubmit} className="">
-        <input type="text" placeholder="Nome" className=""/>
-        <input type="email" placeholder="E-mail" />
-        <input type="password"  placeholder="Senha"/>
-        <input type="password"  placeholder="Confirme senha"/>
-        <input type="submit" value="Cadastrar" />
-      </form>
-      <p>Já tem conta? <Link to="/login">Clique aqui</Link></p>
+    <div className="bg-zinc-950 w-2/5 mx-auto p-4 my-10 rounded-xl">
+      <div className="text-center w-11/12 mx-auto my-0">
+        <h1 className="text-2xl font-bold pb-10 pt-8">Faça seu cadastro</h1>
+        <p className="text-zinc-400 pb-6">Crie sua conta e comece a capturar e compartilhar momentos inesquecíveis.</p>
+        <form onSubmit={handleSubmit} className="border-b border-zinc-700 pb-6">
+          <input type="text" placeholder="Nome" className=""/>
+          <input type="email" placeholder="E-mail" />
+          <input type="password"  placeholder="Senha"/>
+          <input type="password"  placeholder="Confirme senha"/>
+          <input type="submit" value="Cadastrar" />
+        </form>
+        <p className="pt-12 pb-4">Já tem conta? <Link to="/login" className="text-sky-700">Clique aqui</Link></p>
+      </div>
     </div>
   )
 }
