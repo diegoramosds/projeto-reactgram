be3445bf9802c8b50532c750d87a43a90bab9c2f
Diego <diegoramos3709@gmail.com>
2024-09-26 12:24:48 -0300

    Navbar para usuários autenticados

--- frontend/reactGram/src/components/NavBar.tsx
+++ frontend/reactGram/src/components/NavBar.tsx
@@ -4,13 +4,18 @@ import {
   BsFillPersonFill,
   BsFillCameraFill } from "react-icons/bs"
 
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 
 //Hooks
-import {}
+import { useState } from "react"
+import { useAuth } from "../hooks/useAuth"
+import { useDispatch, useSelector } from "react-redux"
 
 
 const NavBar = () => {
+  const {auth} = useAuth();
+  const {user} = useSelector((state) => state.auth);
+
   return (
     <nav className="flex justify-between items-center p-3 border-b border-zinc-800 bg-zinc-950 md:">
       <Link to="/" className="text-zinc-100">ReactGram</Link>
@@ -23,18 +28,38 @@ const NavBar = () => {
         </form>
 
         <ul className="flex gap-2 items-center text-zinc-100 pr-3 md:gap-6 md:text-lg">
-          <li>
-            <NavLink to="/"> <BsHouseDoorFill /> </NavLink>
-          </li>
 
-          <li>
-            <NavLink to="/login"> Entrar </NavLink>
-          </li>
+          {auth ? (
+            <>
+            <li><NavLink to="/"> <BsHouseDoorFill /> </NavLink></li>
 
-          <li>
-            <NavLink to="/register"> Cadastrar </NavLink>
-          </li>
+            {user && (
+              <NavLink to={`/users/${user._id}`}>
+                 <BsFillCameraFill />
+              </NavLink>
+            )}
+
+            <li>
+              <NavLink to="/profile">
+              <BsFillPersonFill />
+              </NavLink>
+            </li>
+
+            <li>
+              <span>Sair</span>
+            </li>
+            </>
           
+
+          )
+          : (
+            <>
+           <li><NavLink to="/login"> Entrar </NavLink> </li> 
+
+          <li> <NavLink to="/register"> Cadastrar </NavLink></li> 
+            </>
+          )
+          }
         </ul>
       
     </nav>
