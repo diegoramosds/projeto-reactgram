6d9c0724b019cbb284c1794e5144f91730893927
Diego <diegoramos3709@gmail.com>
2024-10-07 17:31:28 -0300

    Exibindo dados no perfil

--- frontend/reactGram/src/pages/Profile/Profile.tsx
+++ frontend/reactGram/src/pages/Profile/Profile.tsx
@@ -11,12 +11,43 @@ import { useDispatch, useSelector } from "react-redux"
 
 
 //Redux
-
+import { getUserDetails } from "../../slices/userSlice"
+import { AppDispatch, RootState } from "../../store"
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+
+    const {id} = useParams()
+
+    const dispatch: AppDispatch = useDispatch()
+
+    const {user, loading} = useSelector((state: RootState) => state.user)
+    const {user: userAuth} = useSelector((state: RootState) => state.auth)
+
+    //Photo
+
+    //Load user data
+    useEffect(() => {
+
+        dispatch(getUserDetails(id))
+    }, [dispatch, id])
+
+    if(loading) {
+        return <p>Carregando...</p>
+    }
+
+    return (
+    <div>
+        <div>
+            {user?.profileImage && (
+                <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+            )}
+            <div>
+                <h2>{user?.name}</h2>
+                <p>{user?.bio}</p>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default Profile
