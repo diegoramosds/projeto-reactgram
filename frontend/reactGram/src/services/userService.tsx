9cf5780f1b4be937adb17e1febcf660520d096ad
Diego <diegoramos3709@gmail.com>
2024-10-07 17:30:44 -0300

    Service para resgatar dados do usuário por id

--- frontend/reactGram/src/services/userService.tsx
+++ frontend/reactGram/src/services/userService.tsx
@@ -30,10 +30,26 @@ import { api, requestConfig } from "../utils/config";
     }
  }
 
+ //Get user details
+const getUserDetails = async(id: void) => {
+
+    const config = requestConfig("GET")
+
+    try { 
+        const res = await fetch(api + "/users/" + id, config)
+        .then((res) => res.json())
+        .catch((err) => err);
+
+        return res
+    } catch (error) {
+        console.log(error)
+    }
+}
 
  const userService = {
     profile,
-    update
+    update,
+    getUserDetails
  }
  
  export default userService;
