3067850015477e4761117aba33c98436ecbeadcb
Diego <diegoramos3709@gmail.com>
2024-10-16 17:52:26 -0300

    Service de resgatar foto do usuário

--- frontend/reactGram/src/services/photoService.tsx
+++ frontend/reactGram/src/services/photoService.tsx
@@ -1,5 +1,44 @@
 import { api, requestConfig } from "../utils/config";
 
-const photoService = {}
+const publishPhoto = async(data: string, token: string) => {
+    
+    const config = requestConfig("POST" , data, token, true)
+
+    try {
+        const res = await fetch(api + "/photos", config)
+        .then((res) => res.json())
+        .catch((err) => err);
+
+        return res;
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+//Get user photos
+const getUserPhotos = async(id: string, token: string) => {
+
+    const config = requestConfig("GET", null, token)
+
+
+    try {
+    const res = await fetch(api + "/photos/user/" + id, config)
+    .then((res) => res.json())
+    .catch((err) => err);
+
+    return res;
+        
+    } catch (error) {
+       console.log(error) 
+    }
+}
+
+
+const photoService = {
+    publishPhoto,
+    getUserPhotos
+}
 
 export default  photoService;
