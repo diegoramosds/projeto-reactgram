d6f5d8656257a8a44753ece99500b9dc13050839
Diego <diegoramos3709@gmail.com>
2024-10-18 10:24:14 -0300

    Service de exclusão de fotos

--- frontend/reactGram/src/services/photoService.tsx
+++ frontend/reactGram/src/services/photoService.tsx
@@ -35,10 +35,27 @@ const getUserPhotos = async(id: string, token: string) => {
     }
 }
 
+//Delete a photo
+const deletePhoto = async(id: string, token) => {
+
+     const config = requestConfig("DELETE", null, token)
+
+     try {
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        return res
+
+     } catch (error) {
+        console.log(error)
+     }
+}
 
 const photoService = {
     publishPhoto,
-    getUserPhotos
+    getUserPhotos,
+    deletePhoto
 }
 
 export default  photoService;
