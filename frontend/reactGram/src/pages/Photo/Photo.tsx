b0febbd6311959513f55540e8712ee4a9a2921e8
Diego <diegoramos3709@gmail.com>
2024-10-23 16:52:29 -0300

    Página photos

---
+++ frontend/reactGram/src/pages/Photo/Photo.tsx
@@ -0,0 +1,21 @@
+import { uploads } from "../../utils/config"
+
+//Components
+import Message from "../../components/Message";
+import { Link, useParams } from "react-router-dom";
+
+
+//hooks
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+//redux 
+
+
+const Photo = () => {
+  return (
+    <div>Photo</div>
+  )
+}
+
+export default Photo;
