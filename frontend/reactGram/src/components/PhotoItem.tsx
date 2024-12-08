3eb8d6018fd2ea0962217a588c9168c79ac8f664
Diego <140274064+diegoramosds@users.noreply.github.com>
2024-12-08 20:50:28 -0300

    Componente de photos

--- frontend/reactGram/src/components/PhotoItem.tsx
+++ frontend/reactGram/src/components/PhotoItem.tsx
@@ -8,16 +8,16 @@ interface Photo {
     userName: string;
   }
 
-  
   interface PhotoItemProps {
     photo: Partial<Photo>;
   }
-  
+
   const PhotoItem = ({ photo }: PhotoItemProps)=> {
+
   return (
     <div className="w-full mt-10 flex flex-col gap-6">
         {photo.image && (
-            <img src={`${uploads}/photos/${photo.image}`} alt={photo.title} className="rou"/>
+            <img src={`${uploads}/photos/${photo.image}`} alt={photo.title} className="w-11/12 mx-auto rounded"/>
         )}
         <p className="text-xl font-bold text-center">{photo.title}</p>
         <p className="m-2"> Publicado por: 
