74c30058b675487a4b3aa3566c45dd909e94b82b
Diego <diegoramos3709@gmail.com>
2025-02-19 18:23:42 -0300

    Update Footer.tsx

--- frontend/reactGram/src/components/Footer.tsx
+++ frontend/reactGram/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="bg-zinc-950">
-    <footer className=" flex flex-col justify-center items-center h-48">
+    <div className="bg-zinc-950 mt-20">
+    <footer className=" flex flex-col justify-center items-center h-48 pt">
       <p>ReactGram &copy; 2024</p>
     </footer>
     </div>
