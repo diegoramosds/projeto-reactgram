88afa90794529ecdca74f49427ad1108234482d4
Diego <diegoramos3709@gmail.com>
2025-03-05 22:40:12 -0300

    Update SearchUsers.tsx

--- frontend/reactGram/src/pages/SearchUsers/SearchUsers.tsx
+++ frontend/reactGram/src/pages/SearchUsers/SearchUsers.tsx
@@ -19,6 +19,7 @@ const { user } = useSelector((state: RootState) => state.user);
                 className="w-20 h-20 rounded-full p-4"
               />
             )}
+            
                 <div className="flex justify-center items-center gap-5">
                   <div>
                     <p className="text-lg font-medium">
