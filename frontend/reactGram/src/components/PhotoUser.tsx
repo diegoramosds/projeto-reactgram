4c10e20e929ded6d61dbd21124f518d77e354f85
Diego <diegoramos3709@gmail.com>
2025-03-12 22:03:06 -0300

    Update PhotoUser.tsx

--- frontend/reactGram/src/components/PhotoUser.tsx
+++ frontend/reactGram/src/components/PhotoUser.tsx
@@ -18,6 +18,7 @@ const PhotoUser = ({user}: PhotoUserProps) => {
 
 const dispatch: AppDispatch = useDispatch();
 
+
 return (
     <div className='flex items-center'>
         {user?.profileImage ?  (
