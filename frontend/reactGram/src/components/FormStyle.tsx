e28b85f0d1934583e4972633adc05c1b95c965d6
Diego <diegoramos3709@gmail.com>
2025-03-13 19:49:51 -0300

    Update FormStyle.tsx

--- frontend/reactGram/src/components/FormStyle.tsx
+++ frontend/reactGram/src/components/FormStyle.tsx
@@ -18,9 +18,9 @@ interface FormStyleProps {
 
 const FormStyle = ({ children, title, subtitle, handleSubmit, loading, linkTo, linkText, btnText, errorMessage, successMessage  }: FormStyleProps) => {
   return (
-    <div className="bg-zinc-950 w-2/5 mx-auto p-4 my-10 rounded-xl">
+    <div className="bg-zinc-950 w-2/5 mx-auto p-4 my-4 rounded-xl">
       <div className="text-center w-11/12 mx-auto my-0">
-      <h1 className="text-2xl font-bold pb-10 pt-8">{title}</h1>
+      <h1 className="text-2xl font-bold pb-3">{title}</h1>
       <p className="text-zinc-400 pb-6">{subtitle}</p>
       
       <form onSubmit={handleSubmit} className="border-b border-zinc-700 pb-6">
