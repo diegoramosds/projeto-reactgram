737918b68db6d1934369f7d95e3cb9709691cd10
Diego <diegoramos3709@gmail.com>
2025-03-18 18:51:13 -0300

    Update vite.config.ts

--- frontend/reactGram/vite.config.ts
+++ frontend/reactGram/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Certifique-se de que o diretório de build é 'dist'
+    emptyOutDir: true, // Limpa o diretório de build antes de gerar novos arquivos
+  },
+});
