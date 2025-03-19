c41e4d06b897d58d282ec3c52fed8ee0618aff8c
Diego <diegoramos3709@gmail.com>
2025-03-19 20:43:41 -0300

    Update vite.config.ts

--- frontend/reactGram/vite.config.ts
+++ frontend/reactGram/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    outDir: "dist",
+    emptyOutDir: true
   },
-  base: "/", // Garante que o roteamento funcione na Vercel
+  base: "./" // Garante que os caminhos dos arquivos JS e CSS sejam corretos
 });
