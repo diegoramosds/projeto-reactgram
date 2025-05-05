3c6977ab3ea4e42ad521026abc55f234afa28a98
Diego <diegoramos3709@gmail.com>
2025-05-05 20:52:54 -0300

    Update Footer.tsx

--- frontend/reactGram/src/components/Footer.tsx
+++ frontend/reactGram/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { BiCamera } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div className="bg-black border-t border-zinc-900/30 shadow-sm flex flex-col min-h-[200px]">
+    <div className="bg-black border-t border-zinc-900/30 shadow-sm flex flex-col h-[200px] p-6">
       <footer className="flex flex-col justify-center items-center gap-6 py-10">
         <p className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center gap-1">
           <span>
